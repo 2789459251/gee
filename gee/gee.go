cb9197d9b5bff9896ae47e8f48491b69d1705b5e
2789459251 <18291734409@stu.xupt.edu.cn>
2024-03-05 17:45:17 +0800

    gee路由组添加

--- gee/gee.go
+++ gee/gee.go
@@ -1,19 +1,52 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type Engine struct {
-	router *router
+	*RouterGroup //继承所有字段与方法
+	router       *router
+	groups       []*RouterGroup
 }
 type HandlerFunc func(c *Context)
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
 
 // 新建引擎
 func New() *Engine {
-	return &Engine{
-		router: newRouter(),
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	//初始是加上自己的根分组
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
 	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("路径%s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+
+}
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
 	engine.router.addRoute(method, pattern, handler)
